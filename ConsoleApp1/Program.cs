c6e111453057ae0769843a0c5fc8c4c6d55ae4bd
Kociarz <dbednorz@tuta.io>
2024-12-07 14:14:59 +0100

    Added middle point calculation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,34 +15,38 @@ class Program
         System.Console.Write("Y: ");
         int ya = int.Parse(Console.ReadLine());
         
-        System.Console.WriteLine("Choose your A point coordinates: ");
+        System.Console.WriteLine("Choose your B point coordinates: ");
         System.Console.Write("X: ");
         int xb = int.Parse(Console.ReadLine());
         System.Console.Write("Y: ");
         int yb = int.Parse(Console.ReadLine());
 
         System.Console.WriteLine("Calculating...");
-        pointsDist point = new pointsDist(xa, ya, xb, yb);
-        point.calculate();
+        cordSys pointSys = new cordSys(xa, ya, xb, yb);
+        pointSys.calcDist();
+        pointSys.calcMiddle();
     }
 
-    public struct pointsDist
+    public struct cordSys
     {
         public static int Xa { get; set; }
         public static int Ya { get; set; }
         public static int Xb { get; set; }
         public static int Yb { get; set; }
 
-        public pointsDist(int xa, int ya, int xb, int yb)
+        public cordSys(int xa, int ya, int xb, int yb)
         {
+            // X and Y coordinates for A point
             Xa = xa;
             Ya = ya;
 
+            // X and Y coordinates for B point
             Xb = xb;
             Yb = yb;
         }
 
-        public void calculate(){
+        // Calculate distance between two points
+        public void calcDist(){
             double xResult = Math.Pow(Xb + Xa, 2);
             double yResult = Math.Pow(Yb - Ya, 2);
             double distSqrt = Math.Sqrt(xResult + yResult);
@@ -51,6 +55,16 @@ class Program
             System.Console.WriteLine("Calculation done!");
             System.Console.WriteLine($"{roundedResult} is a distance between point A({Xa}, {Ya}) and B({Xb}, {Yb})");
 
+        }
+
+
+        // Calculate middle point between two points
+        public void calcMiddle(){
+            double xMiddle = (Xa + Xb) / 2;
+            double yMiddle = (Ya + Yb) / 2;
+
+            // System.Console.WriteLine("Calculation done!");
+            System.Console.WriteLine($"Middle point between point A({Xa}, {Ya}) and B({Xb}, {Yb}) is S({xMiddle}, {yMiddle})");        
         }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91f3618dbca9fcb9fa9318dbf6012e91161f42c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
